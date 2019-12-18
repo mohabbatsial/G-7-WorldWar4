32053f89492c50df960feb54dc0bead01eeab6a7
mohabbatsial <alimohabbat45@gmail.com>
2019-12-18 21:36:35 +0500

    WorldWar4

---
+++ Assets/Scripts/MainMenuSounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSounds : MonoBehaviour
+{
+    public AudioSource MainMenuObject;
+    public AudioClip Tank;
+    public AudioClip Jeep;
+    public AudioClip Helicopter;
+    AudioClip[] MainMenuArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuArray = new AudioClip[] { Tank, Jeep, Helicopter};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!MainMenuObject.isPlaying)
+        {
+            int index = Random.Range(0, MainMenuArray.Length);
+            AudioClip clip = MainMenuArray[index];
+            MainMenuObject.clip = clip;
+            MainMenuObject.Play();
+        }
+    }
+}
+
